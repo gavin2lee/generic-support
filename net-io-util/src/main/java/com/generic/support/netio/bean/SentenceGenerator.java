dcc35d3789bc1190e49df21f3b53eb97a1156c5d
gavin <gavin2lee@163.com>
2017-01-12 00:32:05 +0800

    string

--- net-io-util/src/main/java/com/generic/support/netio/bean/SentenceGenerator.java
+++ net-io-util/src/main/java/com/generic/support/netio/bean/SentenceGenerator.java
@@ -1,7 +1,34 @@
 package com.generic.support.netio.bean;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
+
 public class SentenceGenerator {
+	private String[] sentences;
+	
+	private String poemFilename = "changhenge-poem.txt";
+	public SentenceGenerator(){
+		init();
+	}
+	
+	
 	public String generate(){
-		return "hello";
+		Random r = new Random();
+		int index = r.nextInt(sentences.length);
+		return sentences[index];
+	}
+	
+	protected void init(){
+		String path = SentenceGenerator.class.getClassLoader().getResource(poemFilename).getPath();
+		try {
+			List<String> lines = FileUtils.readLines(new File(path), "UTF-8");
+			sentences = (String[]) lines.toArray();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
