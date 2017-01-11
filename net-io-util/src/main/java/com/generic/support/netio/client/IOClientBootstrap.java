798ae91073b838757d44547ad4d44484ec03ce56
gavin <gavin2lee@163.com>
2017-01-11 23:01:44 +0800

    int

--- net-io-util/src/main/java/com/generic/support/netio/client/IOClientBootstrap.java
+++ net-io-util/src/main/java/com/generic/support/netio/client/IOClientBootstrap.java
@@ -1,9 +1,10 @@
 package com.generic.support.netio.client;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.generic.support.netio.client.bio.BioTimeStampEchoClient;
 
 public class IOClientBootstrap {
 	private static final Logger log = LoggerFactory.getLogger(IOClientBootstrap.class);
@@ -17,12 +18,27 @@ public class IOClientBootstrap {
 	private int threads;
 	private int times = 100;
 
+	private String defaultClientClassName = "com.generic.support.netio.client.bio.BioTimeStampEchoClient";
+	private Class<?> clientClass;
+
 	public void boot() {
 		setInitParams();
 		log.debug(String.format("%s [host:%s,port:%d,times:%d]", IOClientBootstrap.class.getSimpleName(), host, port,
 				times));
+		Constructor<?> constructor;
+		try {
+			constructor = clientClass.getConstructor(String.class, int.class, int.class);
+		} catch (NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
 		for (int i = 0; i < threads; i++) {
-			IOClient client = new BioTimeStampEchoClient(host, port, times);
+//			IOClient client = new BioTimeStampEchoClient(host, port, times);
+			IOClient client;
+			try {
+				client = (IOClient) constructor.newInstance(host,port,times);
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException cause){
+				throw new RuntimeException(cause);
+			}
 			new Thread(client).start();
 		}
 	}
@@ -32,6 +48,13 @@ public class IOClientBootstrap {
 		int port = Integer.parseInt(System.getProperty("port", defaultPort));
 		int threads = Integer.parseInt(System.getProperty("threads", defaultThreads));
 		int times = Integer.parseInt(System.getProperty("times", defaultTimes));
+		String clientClassName = System.getProperty("client", defaultClientClassName);
+
+		try {
+			clientClass = Class.forName(clientClassName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 
 		this.host = host;
 		this.port = port;
