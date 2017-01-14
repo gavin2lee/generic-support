2b984c0dba82c0616be9e46815064ad02ee55dc4
gavin2lee <gavin2lee@163.com>
2017-01-14 11:37:07 +0800

    loginserver

--- net-io-util/src/main/java/com/generic/support/netio/server/netty/NettyLoginServer.java
+++ net-io-util/src/main/java/com/generic/support/netio/server/netty/NettyLoginServer.java
@@ -2,12 +2,44 @@ package com.generic.support.netio.server.netty;
 
 import com.generic.support.netio.server.IOServer;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class NettyLoginServer implements IOServer {
 
 	@Override
 	public void start(int port) throws Exception {
-		// TODO Auto-generated method stub
+		EventLoopGroup bossGrp = new NioEventLoopGroup();
+		EventLoopGroup workerGrp = new NioEventLoopGroup();
+		
+		try{
+			ServerBootstrap server = new ServerBootstrap();
+			server.group(bossGrp, workerGrp);
+			server.channel(NioServerSocketChannel.class);
+			server.childHandler(new NettyLoginChannelInitializer());
+			
+			ChannelFuture cf = server.bind(port).sync();
+			cf.channel().closeFuture().sync();
+			
+		}finally{
+			bossGrp.shutdownGracefully();
+			workerGrp.shutdownGracefully();
+		}
 
+	}
+	
+	public class NettyLoginChannelInitializer extends ChannelInitializer<SocketChannel>{
+
+		@Override
+		protected void initChannel(SocketChannel ch) throws Exception {
+			
+		}
+		
 	}
 
 }
