78afe6caf7761e541c473c327960dbd21a6084cb
gavin2lee <gavin2lee@163.com>
2017-01-14 14:44:25 +0800

    netty

--- net-io-util/src/main/java/com/generic/support/netio/client/netty/NettyLoginClient.java
+++ net-io-util/src/main/java/com/generic/support/netio/client/netty/NettyLoginClient.java
@@ -4,10 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.generic.support.netio.client.AbstractMultipleIOClient;
+import com.generic.support.netio.coder.MessagePackDecoder;
+import com.generic.support.netio.coder.MessagePackEncoder;
+import com.generic.support.netio.dto.LoginRequest;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class NettyLoginClient extends AbstractMultipleIOClient {
 	private static final Logger log = LoggerFactory.getLogger(NettyLoginClient.class);
-	
 
 	public NettyLoginClient(String host, int port, int times) {
 		super(host, port, times);
@@ -15,8 +27,74 @@ public class NettyLoginClient extends AbstractMultipleIOClient {
 
 	@Override
 	public void kickoff() throws Exception {
-		// TODO Auto-generated method stub
+		log.debug(String.format("%s  kickoff with host:%s,port:%d", NettyLoginClient.class.getSimpleName(), getHost(),
+				getPort()));
+		EventLoopGroup clientGroup = new NioEventLoopGroup();
 
+		try {
+			Bootstrap client = new Bootstrap();
+			client.group(clientGroup);
+
+			client.channel(NioSocketChannel.class);
+			client.handler(new NettyLoginClientChannelInitializer());
+
+			ChannelFuture cf = client.connect(getHost(), getPort()).sync();
+			cf.channel().closeFuture().sync();
+		} finally {
+			clientGroup.shutdownGracefully();
+		}
+
+	}
+
+	public class NettyLoginClientChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+		@Override
+		protected void initChannel(SocketChannel ch) throws Exception {
+			
+			ch.pipeline().addLast("messagePackDecoder", new MessagePackDecoder());
+			ch.pipeline().addLast("messagePackEncoder", new MessagePackEncoder());
+			
+			ch.pipeline().addLast(new NettyLoginClientHandler());
+		}
+
+	}
+	
+	public class NettyLoginClientHandler extends ChannelInboundHandlerAdapter{
+
+		@Override
+		public void channelActive(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelActive");
+			sendRequest(ctx);
+			ctx.flush();
+		}
+
+		@Override
+		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+			log.debug("channelRead");
+			log.debug("recv type:"+msg.getClass().getName());
+		}
+
+		@Override
+		public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelReadComplete");
+			sendRequest(ctx);
+			ctx.flush();
+		}
+
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+			log.error(cause.getMessage(),cause);
+			ctx.close();
+		}
+		
+		protected void sendRequest(ChannelHandlerContext ctx){
+			LoginRequest req = new LoginRequest();
+			req.setUsername("test");
+			req.setPassword("abc123");
+			
+			ctx.write(req);
+		}
+		
 	}
 
 }
--- net-io-util/src/main/java/com/generic/support/netio/server/netty/NettyLoginServer.java
+++ net-io-util/src/main/java/com/generic/support/netio/server/netty/NettyLoginServer.java
@@ -1,9 +1,19 @@
 package com.generic.support.netio.server.netty;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.generic.support.netio.coder.MessagePackDecoder;
+import com.generic.support.netio.coder.MessagePackEncoder;
+import com.generic.support.netio.dto.LoginResponse;
 import com.generic.support.netio.server.IOServer;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -11,33 +21,96 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class NettyLoginServer implements IOServer {
+	private static final Logger log = LoggerFactory.getLogger(NettyLoginServer.class);
 
 	@Override
 	public void start(int port) throws Exception {
+		log.debug(String.format("%s start with port:%d", NettyLoginServer.class.getName(), port));
 		EventLoopGroup bossGrp = new NioEventLoopGroup();
 		EventLoopGroup workerGrp = new NioEventLoopGroup();
-		
-		try{
+
+		try {
 			ServerBootstrap server = new ServerBootstrap();
 			server.group(bossGrp, workerGrp);
 			server.channel(NioServerSocketChannel.class);
-			server.childHandler(new NettyLoginChannelInitializer());
-			
+			server.childHandler(new NettyLoginServerChannelInitializer());
+
 			ChannelFuture cf = server.bind(port).sync();
 			cf.channel().closeFuture().sync();
-			
-		}finally{
+
+		} finally {
 			bossGrp.shutdownGracefully();
 			workerGrp.shutdownGracefully();
 		}
 
 	}
-	
-	public class NettyLoginChannelInitializer extends ChannelInitializer<SocketChannel>{
+
+	public class NettyLoginServerChannelInitializer extends ChannelInitializer<SocketChannel> {
 
 		@Override
 		protected void initChannel(SocketChannel ch) throws Exception {
+
+			ch.pipeline().addLast("messagePackDecoder", new MessagePackDecoder());
+			ch.pipeline().addLast("messagePackEncoder", new MessagePackEncoder());
 			
+			ch.pipeline().addLast(new NettyLoginServerHandler());
+		}
+
+	}
+
+	public class NettyLoginServerHandler extends ChannelInboundHandlerAdapter {
+
+		@Override
+		public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelRegistered");
+			super.channelRegistered(ctx);
+		}
+
+		@Override
+		public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelUnregistered");
+			super.channelUnregistered(ctx);
+		}
+
+		@Override
+		public void channelActive(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelActive");
+			super.channelActive(ctx);
+		}
+
+		@Override
+		public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelInactive");
+			super.channelInactive(ctx);
+		}
+
+		@Override
+		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+			log.debug("channelRead");
+			log.debug("recv type: "+msg.getClass().getName());
+			
+		}
+
+		@Override
+		public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+			log.debug("channelReadComplete");
+			writeBack(ctx);
+			ctx.flush();
+		}
+
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+			log.error("", cause);
+			ctx.close();
+		}
+		
+		protected void writeBack(ChannelHandlerContext ctx){
+			LoginResponse resp = new LoginResponse();
+			resp.setUserid(1L);
+			resp.setUsername("server-1");
+			resp.setRoles(Arrays.asList(new String[]{"user", "admin"}));
+			
+			ctx.write(resp);
 		}
 		
 	}
