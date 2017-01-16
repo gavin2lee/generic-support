fc7fe7307940c8ce5e0606bc9edf8c3ff89dd59e
gavin2lee <gavin2lee@163.com>
2017-01-16 19:44:41 +0800

    test

--- net-io-util/src/test/java/com/generic/support/WindowsFileOperationsTest.java
+++ net-io-util/src/test/java/com/generic/support/WindowsFileOperationsTest.java
@@ -9,12 +9,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class WindowsFileOperationsTest {
+public class  WindowsFileOperationsTest{
 	private static final Logger log = LoggerFactory.getLogger(WindowsFileOperationsTest.class);
 
 	@Test
 	public void testListDisk(){
-		String baseDir = "D";
+		String baseDir = "D:\\";
+		String os = System.getProperty("os.name");
+		log.debug("OS:"+os);
+		if(!os.startsWith("Windows")){
+		    log.debug(WindowsFileOperationsTest.class.getName() + " test skipped");
+		    return;
+		}
 		log.debug("list all children files under "+baseDir);
 		File dir = new File(baseDir);
 		Assert.assertThat(dir, Matchers.notNullValue());
