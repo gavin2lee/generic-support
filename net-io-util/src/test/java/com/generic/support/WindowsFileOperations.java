758b021c869ad2883bacaae860a4e000ba8b22a2
gavin2lee <gavin2lee@163.com>
2017-01-16 18:42:55 +0800

    win

---
+++ net-io-util/src/test/java/com/generic/support/WindowsFileOperations.java
@@ -0,0 +1,21 @@
+package com.generic.support;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WindowsFileOperations {
+	private static final Logger log = LoggerFactory.getLogger(WindowsFileOperations.class);
+
+	@Test
+	public void testListDisk(){
+		String baseDir = "D";
+		File dir = new File(baseDir);
+		for(File childF : dir.listFiles()){
+			log.debug(childF.getName());
+		}
+	}
+
+}
