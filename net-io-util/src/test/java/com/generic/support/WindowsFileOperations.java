7f0dc04a2a86587bb9dcc6331a1e7c3171238266
gavin2lee <gavin2lee@163.com>
2017-01-16 19:27:54 +0800

    test

--- net-io-util/src/test/java/com/generic/support/WindowsFileOperations.java
+++ net-io-util/src/test/java/com/generic/support/WindowsFileOperations.java
@@ -2,6 +2,8 @@ package com.generic.support;
 
 import java.io.File;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,7 @@ public class WindowsFileOperations {
 		String baseDir = "D";
 		log.debug("list all children files under "+baseDir);
 		File dir = new File(baseDir);
+		Assert.assertThat(dir, Matchers.notNullValue());
 		for(File childF : dir.listFiles()){
 			log.debug(childF.getName());
 		}
